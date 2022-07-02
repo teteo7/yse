9196398d7eef4567bb17b93d5f6fba571069f65e
teteo7 <teteocomputer@gmail.com>
2022-07-03 08:21:23 +0900

    Intellij commit

---
+++ yse/src/test/java/sample/spring/yse/SampleSpringYseApplicationTests.java
@@ -0,0 +1,13 @@
+package sample.spring.yse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleSpringYseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
